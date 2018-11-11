ef32be3278211c00472abefe9864fe36f363e031
Andreas Mock <andreas.mock@live.com>
2018-11-11 17:46:59 +0100

    removed some scripts

--- src/deploy/ServiceFabricAppGtw.Iac/Connect-Cluster.ps1
+++
@@ -1,5 +0,0 @@
-#
-# Connect_Cluster.ps1
-#
-$ConnectArgs = @{  ConnectionEndpoint = 'sfdevopscluster.westeurope.cloudapp.azure.com:19000';  X509Credential = $True;  StoreLocation = 'CurrentUser';  StoreName = "MY";  ServerCommonName = "sfdevopscluster.westeurope.cloudapp.azure.com";  FindType = 'FindByThumbprint';  FindValue = "35227E876353B6F2BDCD1C2639F08DCF64B6F667"   }
-Connect-ServiceFabricCluster @ConnectArgs
